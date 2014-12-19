8a7ff160996d0243438f2af19afb7597bd589963
Aris FM <masoglek@gmail.com>
2014-12-20 01:45:39 +0700

    Adding .editorconfig file

--- dotfiles.sh
+++ dotfiles.sh
@@ -6,10 +6,10 @@ function load_all() {
   file_type=$1
   [ ! -d "$path/$file_type" ] && mkdir "$path/${file_type}"
   for src in $path/${file_type}/*; do
-      filename="$(basename ${src})"
-      [ ${filename:0:1} = "_" ] && continue
-      dest="${path}/${file_type}/${filename}"
-      source $dest
+    filename="$(basename ${src})"
+    [ ${filename:0:1} = "_" ] && continue
+    dest="${path}/${file_type}/${filename}"
+    source $dest
   done
 }
 
