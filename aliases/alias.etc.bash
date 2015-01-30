29f1fad8d7cc2aa0180b80fb3d48f013f19b229c
Aris FM <masoglek@gmail.com>
2015-01-31 00:53:11 +0700

    New aliases: Ruby
    - Middleman
    - Bundler
    - Rake

--- aliases/alias.etc.bash
+++ aliases/alias.etc.bash
@@ -4,10 +4,6 @@
 alias st='"/c/Program Files/Sublime Text 3/sublime_text.exe"'
 alias subl='"/c/Program Files/Sublime Text 3/sublime_text.exe"'
 
-# Middleman
-alias mid='middleman'
-alias mid-b='middleman build'
-
 # File navigation
 alias md='mkdir -p'
 alias rd='rmdir'
---
+++ aliases/alias.ruby.bash
@@ -0,0 +1,12 @@
+# Middleman
+alias mid='middleman'
+alias midb='middleman build'
+alias mida='middleman article'
+
+# Bundler
+alias bun='bundle install'
+alias bunl='bundle list'
+alias bunu='bundle update'
+
+# Rake
+alias publish='rake publish'
