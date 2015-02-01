301327408422034a3f0093d16b19e7a27ada4588
Aris FM <masoglek@gmail.com>
2015-02-02 00:52:06 +0700

    sublime text function (previously alias)

--- plugins/plugin.system.bash
+++ plugins/plugin.system.bash
@@ -7,3 +7,9 @@ ren() {
 prefix() {
   for filename in *$1; do mv "$filename" "$2$filename"; done
 }
+
+# Open Sublime Text
+subl() {
+  "/c/Program Files/Sublime Text 3/sublime_text.exe" $* &
+}
+alias st=subl
