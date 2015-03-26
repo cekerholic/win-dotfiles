8fd9bac1954c0afb5a0559ccbd6f0e89787e7503
Aris FM <masoglek@gmail.com>
2015-03-26 12:38:02 +0700

    add some new git aliases

--- aliases/alias.git.bash
+++ aliases/alias.git.bash
@@ -2,16 +2,20 @@
 alias g='git'
 alias ga='git add'
 alias gb='git branch'
+alias gbd='git branch -d'
 alias gc='git commit -v'
 alias gck='git checkout'
 alias gckb='git checkout -b'
 alias gd='git diff'
-alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gl1="git log --oneline"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset' --abbrev-commit --date=relative"
+alias gl1="git log --format='%Cred%h%Creset %s %Cgreen(%cr) %C(blue)<%an>%Creset%C(yellow)%d%Creset' --no-merges"
+alias gls="git log --stat"
 alias gm='git merge --no-ff'
 alias gpl='git pull'
 alias gpu='git push'
-alias gst='git status'
+alias gst='git status -sb'
 alias gr='git reset HEAD --'
 alias gw='git whatchanged --oneline'
+alias gremote='git remote -v'
+alias gt='git tag'
 
